49dfdef6933d1afe05dc2910cec0a1b7cc7c98d4
LandonDaBird <LandonBird09@icloud.com>
2026-03-02 18:10:50 +0000

    Mon, Mar 2, 2026, 10:10 AM -08:00

--- script/expanding.js
+++ script/expanding.js
@@ -2,6 +2,6 @@ panels = document.querySelectorAll('.panel')
 
 for (i = 0; i < 5; i = i + 1){
     panels[i].addEventListener('click', () => {
-        panels[i].classlist.add('active')
+        panels[i].classList.add('active')
     })
 }
