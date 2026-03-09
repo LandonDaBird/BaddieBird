3471e26fdb5e775f8c8811eef414c21d244b20e6
LandonDaBird <LandonBird09@icloud.com>
2026-03-09 16:41:22 +0000

    Mon, Mar 9, 2026, 9:41 AM -07:00

--- script/interactivewebpage.js
+++ script/interactivewebpage.js
@@ -1,4 +1,10 @@
 box = document.querySelectorAll('.box')
 
 
+box.addEventListener('mouseenter', () => {
+    h3 =  opacity 1;
+})
 
+box.addEventListener('mouseleave', () => {
+    h3 = opacity 0;
+})
